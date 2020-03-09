e1f2debce293f3c4bf01dfab1e6b275e9545510c
Gleb Stashinsky <tragy@list.ru>
2020-03-09 13:09:35 +0300

    Create DebtorComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DebtorComponent } from './debtor/debtor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DebtorComponent
   ],
   imports: [
     BrowserModule,
